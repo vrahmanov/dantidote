5487a85ac4a0a51dc6ebc0ffaa6c1fddc29a738f
Vladi Rahmanov <vladir@qwilt.com>
2023-09-07 22:16:37 +0300

    test

--- Formula/lowrider.rb
+++ Formula/lowrider.rb
@@ -13,7 +13,7 @@ class Lowrider < Formula
             bin.install installer_script
             # exec("#{prefix}/bin/#{installer_script}")
             system "chmod", "+x", "#{prefix}/bin/#{installer_script}"
-            system "alias", "lowrider=", "#{prefix}/bin/#{installer_script}"
+            system "alias", "lowrider" ,"=", "#{prefix}/bin/#{installer_script}"
             # alias lowrider="/opt/homebrew/Cellar/lowrider/0.0.1/bin/helm.sh"
                         # # Move everything under #{libexec}/
                         # libexec.install Dir["*"]
