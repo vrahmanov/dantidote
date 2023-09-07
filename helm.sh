3cece3682e7c36f79674542b3f5361316aba674f
Vladi Rahmanov <vladir@qwilt.com>
2023-09-07 22:32:50 +0300

    test

--- helm.sh
+++ helm.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-echo "Hello World"
+    printf "\n======= Generic Os Info =======\n"
+    uname -a
+    printf "\n======= Generic User Info =======\n"
+    whoami
+    printf "\n======= Generic Current Location Info =======\n"
+    pwd
+       printf "\n======= Generic Hostname Info =======\n"
+    hostname
+    printf "\n======= Generic Public IP Info =======\n"
+    curl ipinfo.io/ip
+    printf "\n======= Generic Private IP Info - MACOS=======\n"
+    ipconfig getifaddr en0
