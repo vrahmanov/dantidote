779d56ba848e42bf35c8340c8ab99bca27f4f945
Vladi Rahmanov <vladir@qwilt.com>
2023-09-07 21:59:20 +0300

    teest

--- Formula/lowrider.rb
+++ Formula/lowrider.rb
@@ -7,13 +7,17 @@ class Lowrider < Formula
         if Hardware::CPU.arm?
           url "https://raw.githubusercontent.com/vrahmanov/dantidote/main/helm.sh"
           def install
-                        # Move everything under #{libexec}/
-                        libexec.install Dir["*"]
-                        # Then write executables under #{bin}/
-                        bin.write_exec_script (libexec/"test.sh")
+            puts "inside install"
+            puts %x(whoami)
+            installer_script = "helm.sh"
+            bin.install installer_script
+            exec("#{prefix}/bin/#{installer_script}")
+         
+                        # # Move everything under #{libexec}/
+                        # libexec.install Dir["*"]
+                        # # Then write executables under #{bin}/
+                        # bin.write_exec_script (libexec/"test.sh")
           end
         end
       end
-    
-
 end
