8e465d341799b1f8461cdee4fb50351872b97045
Vladi Rahmanov <vladir@qwilt.com>
2023-09-08 08:31:30 +0300

    test

--- Formula/lowrider.rb
+++ Formula/lowrider.rb
@@ -6,11 +6,12 @@ class Lowrider < Formula
     head "https://github.com/vrahmanov/dantidote.git", branch: "main" # the default is "master"
     on_macos do
         if Hardware::CPU.arm?
-          url "https://raw.githubusercontent.com/vrahmanov/dantidote/main/helm.sh"
+          #url "https://raw.githubusercontent.com/vrahmanov/dantidote/main/helm.sh"
+          url "https://gist.githubusercontent.com/vrahmanov/2dd662a6628f06dd5561428afa990582/raw/a14a82450765a912d7baf797bdeb5132b95925fa/brew_gist.sh"
           def install
             puts "inside install"
             puts %x(whoami)
-            installer_script = "helm.sh"
+            installer_script = "brew_gist.sh"
             bin.install installer_script
             # exec("#{prefix}/bin/#{installer_script}")
             system "chmod", "+x", "#{prefix}/bin/#{installer_script}"
