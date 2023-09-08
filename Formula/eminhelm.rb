0bf51ada2d9ff8d8c19508287911540b3e709dfb
Vladi Rahmanov <vladir@qwilt.com>
2023-09-08 08:52:01 +0300

    test

--- Formula/eminhelm.rb
+++ Formula/eminhelm.rb
@@ -1,9 +1,9 @@
 class Eminhelm < Formula
   # ...
-  desc "Schema registry"
   homepage "https://github.com/vrahmanov"
   version "0.0.2"
   head "https://github.com/vrahmanov/dantidote.git", branch: "main" # the default is "master"
+  desc "A small tap 🐐 for helm chart version display (current vs latest)"
   on_macos do
       if Hardware::CPU.arm?
         url "https://gist.githubusercontent.com/vrahmanov/aa5ad4f44997ac5143b4d233fe8d7023/raw/f684cf6e20da7df4a9c6789283ba68e12b4b3533/helm_split.sh"
